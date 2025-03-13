adf1f48ab174b8a905ff4f44a882c8474a64d464
demetre03 <nikolaishvilidemetre29@gmail.com>
2025-03-13 18:38:11 +0400

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,3 @@
+num = float(input("ჩაწერეთ რიცხვი: "))
+
+print(num >= 5 and num <= 10)
---
+++ day 08/homework/homework2.py
@@ -0,0 +1,3 @@
+True and False or False or True and False     # გამოიტანს False
+
+print(True and False or False or True and False)
