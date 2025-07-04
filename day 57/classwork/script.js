449467b76bdcf885b8ffb16c5e8545be87ef6bba
demetre03 <nikolaishvilidemetre29@gmail.com>
2025-07-04 12:38:51 +0400

    commit

---
+++ day 57/classwork/script.js
@@ -0,0 +1,24 @@
+/*
+1)     let car = "mercedes";
+
+if (car === "mercedes") {
+  console.log("good choice");
+} else if (car === "tesla") {
+  console.log("bad choice");
+} else if (car === "opel") {
+  console.log("mid choice");
+} else {
+  console.log("I can't tell");
+} 
+
+   
+
+2)     let favoriteFilm = "django";
+
+let filmList = ["inception", "interstellar", "the Matrix", "fight Club", "django"];
+
+if (filmList.includes(favoriteFilm)) {
+  console.log("it is a good film");
+} else {
+  console.log("the film is not good");
+}
