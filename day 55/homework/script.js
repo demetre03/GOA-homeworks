85b5cc31d764b6207339c29f806650b4cd25c8f2
demetre03 <nikolaishvilidemetre29@gmail.com>
2025-07-05 15:23:19 +0400

    commit

---
+++ day 55/homework/script.js
@@ -0,0 +1,34 @@
+// მიმატება
+console.log(2 + 3);
+console.log(10 + 20);
+console.log(7 + 1);
+console.log(0 + 9);
+console.log(100 + 5);
+
+// გამოკლება
+console.log(10 - 5);
+console.log(20 - 3);
+console.log(8 - 10);
+console.log(0 - 7);
+console.log(100 - 50); 
+
+// გამრავლება
+console.log(2 * 3);
+console.log(5 * 0);
+console.log(10 * 10);
+console.log(-4 * 2);
+console.log(6 * 7);
+
+// გაყოფა
+console.log(10 / 2);
+console.log(9 / 3);
+console.log(5 / 2);
+console.log(100 / 4);
+console.log(1 / 2);
+
+// ნაშთიანი გაყოფა
+console.log(10 % 3);
+console.log(9 % 2);
+console.log(15 % 4);
+console.log(6 % 3);
+console.log(7 % 5);
---
+++ day 57/homework/script.js
@@ -0,0 +1,8 @@
+const button1 = document.getElementById("button1")
+
+const heading1 = document.getElementById("heading1")
+
+button1.addEventListener("click", function(){
+    heading1.textContent = "hello world"
+})
+
